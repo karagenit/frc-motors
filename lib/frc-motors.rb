9aec3ad4a0f6a819d0d339c664fc2409f8fec288
Caleb Smith <karagenit@outlook.com>
2018-02-07 12:24:37 -0500

    Read File with Headers

--- lib/frc-motors.rb
+++ lib/frc-motors.rb
@@ -1,9 +1,13 @@
 require 'csv'
 
+def get_path(filename)
+  File.dirname(File.expand_path(__FILE__))+"/../data/#{filename}.csv"
+end
+
 class Motor # TODO: move to module
   def initialize(filename)
-    @data = CSV.read(File.dirname(File.expand_path(__FILE__))+"/../data/#{filename}.csv")
-    p @data
+    @data = CSV.read(get_path(filename), headers: true)
+    p @data[0]
   end
 
   def find(speed: nil, torque: nil, current: nil, input_power: nil, output_power: nil, efficiency: nil, lost_power: nil)
