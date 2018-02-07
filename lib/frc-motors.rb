7595ca3c3dba845b223f2f5741e9f0d0070ead6d
Caleb Smith <karagenit@outlook.com>
2018-02-07 12:48:01 -0500

    Move Motor Class into Motors Module

--- lib/frc-motors.rb
+++ lib/frc-motors.rb
@@ -1,33 +1,33 @@
 require 'csv'
 
-def get_path(filename)
-  File.dirname(File.expand_path(__FILE__))+"/../data/#{filename}.csv"
-end
-
-class Motor # TODO: move to module
-
-  attr_reader :data
-
-  def initialize(filename)
-    @data = CSV.read(get_path(filename), headers: true)
-  end
-
-  def find(colname, value)
-    value = value.to_i
-    colname = colname.to_s
-
-    best_row = @data[0]
-
-    @data.each do |row|
-      if (row[colname].to_i - value).abs < (best_row[colname].to_i - value).abs
-        best_row = row
-      end
-    end
-
-    best_row
-  end
-end
 
 module Motors
+  class Motor
+    attr_reader :data
+
+    def initialize(filename)
+      @data = CSV.read(get_path(filename), headers: true)
+    end
+
+    def find(colname, value)
+      value = value.to_i
+      colname = colname.to_s
+
+      best_row = @data[0]
+
+      @data.each do |row|
+        if (row[colname].to_i - value).abs < (best_row[colname].to_i - value).abs
+          best_row = row
+        end
+      end
+
+      best_row
+    end
+
+    def get_path(filename)
+      File.dirname(File.expand_path(__FILE__))+"/../data/#{filename}.csv"
+    end
+  end
+
   CIM = Motor.new("cim")
 end
