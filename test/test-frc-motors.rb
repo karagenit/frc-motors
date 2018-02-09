abcdd47fa34636709091b489b809c5a83a3e7f14
Caleb Smith <karagenit@outlook.com>
2018-02-08 22:05:38 -0500

    Add MiniCIM Tests

--- test/test-frc-motors.rb
+++ test/test-frc-motors.rb
@@ -6,6 +6,13 @@ require 'test/unit'
 class TestQuery < Test::Unit::TestCase
   def test_cim
     assert_not_nil Motors::CIM
+    assert_not_nil Motors::CIM.data
     assert_not_nil Motors::CIM.find(:torque, 1.5)
+  end
+
+  def test_mini_cim
+    assert_not_nil Motors::MiniCIM
+    assert_not_nil Motors::MiniCIM.data
+    assert_not_nil Motors::MiniCIM.find(:torque, 0.7)
   end
 end
